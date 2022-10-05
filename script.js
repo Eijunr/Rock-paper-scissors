735d26841f9dc073b9efabda6eed5f8f4e41362f
Enjunr <ernicbt@gmail.com>
2022-10-04 21:07:18 -0300

    Add function that plays the game five times

--- script.js
+++ script.js
@@ -1,9 +1,66 @@
+//Takes the player move
+let playerMove = prompt().toLowerCase()
+const computerSelection = getComputerChoice()
+
+//score announcement
+const win = "YOU WIN!"
+const lose = "YOU LOST!"
+const tie = "ITS A TIE!"
+
 //Randomly chosses between the game
 function getComputerChoice () {
     let options = ['rock', 'paper', 'scissors'];
-    let randomOption = Math.floor(Math.random() * options.length);
-    let compMove = options[randomOption]
-    return compMove
+    let randomOption = options[Math.floor(Math.random() * options.length)];
+    return randomOption
 }
 
 
+//plays a round of rock paper scissors
+function playRound (playerMove, computerSelection) {
+    switch (playerMove) {
+        case 'rock':
+            if (computerSelection === "rock") {
+                return tie;
+            }
+            else if (computerSelection === "scissors") {
+                return win;
+            }
+            else {
+                return lose;
+            }
+            break
+        case 'paper':
+            if (computerSelection === 'paper') {
+                return tie;
+            }
+            else if (computerSelection === 'rock') {
+                return win;
+            }
+            else {
+                return lose;
+            }
+            break
+        case 'scissors':
+            if (computerSelection === 'scissors') {
+                return tie;
+            }
+            else if (computerSelection === 'paper') {
+                return win;
+            }
+            else {
+                return lose;
+            }
+            break
+            default: console.log("invalid move")
+        }
+}
+//function that plays 5 round
+function round5 () {
+    for (let i = 0; i < 5; i++) {
+        let playerMove = prompt().toLowerCase()
+        getComputerChoice()
+        const computerSelection = getComputerChoice()
+        playRound(playerMove, computerSelection)
+        console.log(playRound(playerMove, computerSelection))
+    }
+}
